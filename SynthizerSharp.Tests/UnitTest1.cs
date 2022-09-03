f11e1e96c9b11135c674a40f5674d69c2a0a0503
Ethin Probst <harlydavidsen@gmail.com>
2022-09-02 20:16:09 -0500

    Add automation functions to `IRawSynthizer` and add remaining unions for automation batches. Change `T[]` to either `Span<T>` or `Memory<T>` depending on usage.

--- SynthizerSharp.Tests/UnitTest1.cs
+++ SynthizerSharp.Tests/UnitTest1.cs
@@ -2,136 +2,166 @@ namespace SynthizerSharp.Tests;
 
 public class UnitTest1
 {
-// OS platforms were found in NUnits source code: https://raw.githubusercontent.com/nunit/nunit/master/src/NUnitFramework/framework/Internal/PlatformHelper.cs
-[Platform("Linux,Unix")]
-[Test]
-public void TestUnixLibraryActivation(){
-Assert.DoesNotThrow(() => {
-var activator = new NativeLibraryBuilder();
-var library = activator.ActivateInterface<IRawSynthizer>("libsynthizer.so");
-});
-Assert.Pass();
-}
+    // OS platforms were found in NUnits source code: https://raw.githubusercontent.com/nunit/nunit/master/src/NUnitFramework/framework/Internal/PlatformHelper.cs
+    [Platform("Linux,Unix")]
+    [Test]
+    public void TestUnixLibraryActivation()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            var options = ImplementationOptions.UseLazyBinding | ImplementationOptions.GenerateDisposalChecks | ImplementationOptions.UseIndirectCalls;
+            var activator = new NativeLibraryBuilder(options);
+            var library = activator.ActivateInterface<IRawSynthizer>("libsynthizer.so");
+        });
+        Assert.Pass();
+    }
 
-[Platform("MacOSX")]
-[Test]
-public void TestAppleLibraryActivation(){
-Assert.DoesNotThrow(() => {
-var activator = new NativeLibraryBuilder();
-var library = activator.ActivateInterface<IRawSynthizer>("synthizer.dylib");
-});
-Assert.Pass();
-}
+    [Platform("MacOSX")]
+    [Test]
+    public void TestAppleLibraryActivation()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            var options = ImplementationOptions.UseLazyBinding | ImplementationOptions.GenerateDisposalChecks | ImplementationOptions.UseIndirectCalls;
+            var activator = new NativeLibraryBuilder(options);
+            var library = activator.ActivateInterface<IRawSynthizer>("synthizer.dylib");
+        });
+        Assert.Pass();
+    }
 
-[Platform("Win7,Windows7,Win8,Windows8,Win8.1,Windows8.1,Windows10,Win10,Xbox")]
-[Test]
-public void TestWin32LibraryActivation() {
-Assert.DoesNotThrow(() => {
-var activator = new NativeLibraryBuilder();
-var library = activator.ActivateInterface<IRawSynthizer>("synthizer.dll");
-});
-Assert.Pass();
-}
+    [Platform("Win7,Windows7,Win8,Windows8,Win8.1,Windows8.1,Windows10,Win10,Xbox")]
+    [Test]
+    public void TestWin32LibraryActivation()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            var options = ImplementationOptions.UseLazyBinding | ImplementationOptions.GenerateDisposalChecks | ImplementationOptions.UseIndirectCalls;
+            var activator = new NativeLibraryBuilder(options);
+            var library = activator.ActivateInterface<IRawSynthizer>("synthizer.dll");
+        });
+        Assert.Pass();
+    }
 
-[Test]
-public void TestDoubleRefcount() {
-var library = FFIActivator.ActivateFFIInterface();
+    [Test]
+    public void TestDoubleRefcount()
+    {
+        var library = FFIActivator.ActivateFFIInterface();
         int h;
         Assert.That(library.syz_initialize(), Is.EqualTo(0));
         Assert.That(library.syz_createContext(out h, null, null), Is.EqualTo(0));
-        Assert.That(library.syz_handleIncRef(h), Is.EqualTo(0));
-        Assert.That(library.syz_handleDecRef(h), Is.EqualTo(0));
-        Assert.That(library.syz_handleDecRef(h), Is.EqualTo(0));
+        Assert.That(library.syz_handleIncRef(ref h), Is.EqualTo(0));
+        Assert.That(library.syz_handleDecRef(ref h), Is.EqualTo(0));
+        Assert.That(library.syz_handleDecRef(ref h), Is.EqualTo(0));
         Assert.That(library.syz_shutdown(), Is.EqualTo(0));
         Assert.Pass();
-}
+    }
 
-[Test]
-public void TestEffectConnections() {
-var library = FFIActivator.ActivateFFIInterface();
-int ctx;
-var sources = new int[3];
-var reverbs = new int[2];
-RouteConfig cfg = new();
-cfg.Gain = 1.0;
-cfg.FadeTime = 0.1;
-Assert.That(library.syz_initialize(), Is.EqualTo(0));
-Assert.That(library.syz_createContext(out ctx, null, null), Is.EqualTo(0));
-for (int i = 0; i < sources.Length; ++i) {
-Assert.That(library.syz_createSource3D(out sources[i], ctx, PannerStrategy.Delegate, 0.0, 0.0, 0.0, null, null, null), Is.EqualTo(0));
-}
-for (int i = 0; i < reverbs.Length; ++i) {
-Assert.That(library.syz_createGlobalFdnReverb(out reverbs[i], ctx, null, null, null), Is.EqualTo(0));
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
-}
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.01), Is.EqualTo(0));
-}
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
-Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.05), Is.EqualTo(0));
-}
-}
-foreach(var source in sources) {
-Assert.That(library.syz_handleDecRef(source), Is.EqualTo(0));
-}
-foreach(var reverb in reverbs) {
-Assert.That(library.syz_handleDecRef(reverb), Is.EqualTo(0));
-}
-Assert.That(library.syz_shutdown(), Is.EqualTo(0));
-Assert.Pass();
-}
+    [Test]
+    public void TestEffectConnections()
+    {
+        var library = FFIActivator.ActivateFFIInterface();
+        int ctx;
+        var sources = new int[3];
+        var reverbs = new int[2];
+        RouteConfig cfg = new();
+        cfg.Gain = 1.0;
+        cfg.FadeTime = 0.1;
+        Assert.That(library.syz_initialize(), Is.EqualTo(0));
+        Assert.That(library.syz_createContext(out ctx, null, null), Is.EqualTo(0));
+        for (int i = 0; i < sources.Length; ++i)
+        {
+            Assert.That(library.syz_createSource3D(out sources[i], ctx, PannerStrategy.Delegate, 0.0, 0.0, 0.0, null, null, null), Is.EqualTo(0));
+        }
+        for (int i = 0; i < reverbs.Length; ++i)
+        {
+            Assert.That(library.syz_createGlobalFdnReverb(out reverbs[i], ctx, null, null, null), Is.EqualTo(0));
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
+            }
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.01), Is.EqualTo(0));
+            }
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
+                Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.05), Is.EqualTo(0));
+            }
+        }
+        for (int i = 0; i < sources.Length; ++i)
+        {
+            Assert.That(library.syz_handleDecRef(ref sources[i]), Is.EqualTo(0));
+        }
+        for (int i = 0; i < reverbs.Length; ++i)
+        {
+            Assert.That(library.syz_handleDecRef(ref reverbs[i]), Is.EqualTo(0));
+        }
+        Assert.That(library.syz_shutdown(), Is.EqualTo(0));
+        Assert.Pass();
+    }
 
-// Significantly extended (in quantity) version of the above test to stress-test effect connections
-/*
-[Test]
-public void TestEffectConnectionsExtended() {
-var library = FFIActivator.ActivateFFIInterface();
-int ctx;
-var sources = new int[250000000];
-var reverbs = new int[250000000];
-RouteConfig cfg = new();
-cfg.Gain = 1.0;
-cfg.FadeTime = 0.1;
-Assert.That(library.syz_initialize(), Is.EqualTo(0));
-Assert.That(library.syz_createContext(out ctx, null, null), Is.EqualTo(0));
-for (int i = 0; i < sources.Length; ++i) {
-Assert.That(library.syz_createSource3D(out sources[i], ctx, PannerStrategy.Delegate, 0.0, 0.0, 0.0, null, null, null), Is.EqualTo(0));
-}
-for (int i = 0; i < reverbs.Length; ++i) {
-Assert.That(library.syz_createGlobalFdnReverb(out reverbs[i], ctx, null, null, null), Is.EqualTo(0));
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
-}
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.01), Is.EqualTo(0));
-}
-}
-foreach (var source in sources) {
-foreach (var reverb in reverbs) {
-Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
-Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.05), Is.EqualTo(0));
-}
-}
-foreach(var source in sources) {
-Assert.That(library.syz_handleDecRef(source), Is.EqualTo(0));
-}
-foreach(var reverb in reverbs) {
-Assert.That(library.syz_handleDecRef(reverb), Is.EqualTo(0));
-}
-Assert.That(library.syz_shutdown(), Is.EqualTo(0));
-Assert.Pass();
-}
-*/
+    // Significantly extended (in quantity) version of the above test to stress-test effect connections
+    [Test]
+    public void TestEffectConnectionsExtended()
+    {
+        var library = FFIActivator.ActivateFFIInterface();
+        int ctx;
+        var sources = new int[250000000];
+        var reverbs = new int[250000000];
+        RouteConfig cfg = new();
+        cfg.Gain = 1.0;
+        cfg.FadeTime = 0.1;
+        Assert.That(library.syz_initialize(), Is.EqualTo(0));
+        Assert.That(library.syz_createContext(out ctx, null, null), Is.EqualTo(0));
+        for (int i = 0; i < sources.Length; ++i)
+        {
+            Assert.That(library.syz_createSource3D(out sources[i], ctx, PannerStrategy.Delegate, 0.0, 0.0, 0.0, null, null, null), Is.EqualTo(0));
+        }
+        for (int i = 0; i < reverbs.Length; ++i)
+        {
+            Assert.That(library.syz_createGlobalFdnReverb(out reverbs[i], ctx, null, null, null), Is.EqualTo(0));
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
+            }
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.01), Is.EqualTo(0));
+            }
+        }
+        foreach (var source in sources)
+        {
+            foreach (var reverb in reverbs)
+            {
+                Assert.That(library.syz_routingConfigRoute(ctx, source, reverb, ref cfg), Is.EqualTo(0));
+                Assert.That(library.syz_routingRemoveRoute(ctx, source, reverb, 0.05), Is.EqualTo(0));
+            }
+        }
+        for (int i = 0; i < sources.Length; ++i)
+        {
+            Assert.That(library.syz_handleDecRef(ref sources[i]), Is.EqualTo(0));
+        }
+        for (int i = 0; i < reverbs.Length; ++i)
+        {
+            Assert.That(library.syz_handleDecRef(ref reverbs[i]), Is.EqualTo(0));
+        }
+        Assert.That(library.syz_shutdown(), Is.EqualTo(0));
+        Assert.Pass();
+    }
 }
--- SynthizerSharp/Synthizer.FFI.cs
+++ SynthizerSharp/Synthizer.FFI.cs
@@ -15,12 +15,32 @@ public struct UserAutomationEventPayload
     public UserAutomationEvent UserAutomation;
 }
 
-public struct Event
+public struct AutomationEvent
 {
-    public int Type;
+    public EventType Type;
     public int Source;
     public int Context;
     public UserAutomationEventPayload Payload;
+}
+
+[StructLayout(LayoutKind.Explicit, Pack = 1)]
+public struct AutomationCommandParameters
+{
+    [FieldOffset(0)]
+    public AutomationAppendPropertyCommand AppendToProperty;
+    [FieldOffset(sizeof(AutomationAppendPropertyCommand))]
+    public AutomationClearPropertyCommand ClearProperty;
+    [FieldOffset(sizeof(AutomationAppendPropertyCommand) + sizeof(AutomationClearPropertyCommand))]
+    public AutomationSendUserEventCommand SendUserEvent;
+}
+
+public struct automationCommandData
+{
+    public int Target;
+    public double Time;
+    public AutomationCommand Command;
+    public uint Flags;
+    public AutomationCommandParameters Parameters;
 }
 
 public enum LoggingBackend
@@ -63,21 +83,20 @@ public struct BiquadConfig
 
 public struct AutomationPoint
 {
-public int InterpolationType;
-[MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
-    public double[] Values;
+    public int InterpolationType;
+    public Span<double> Values;
     public ulong Flags;
 }
 
 public struct AutomationAppendPropertyCommand
 {
-    public int Property;
+    public Property Property;
     public AutomationPoint Point;
 }
 
 public struct AutomationClearPropertyCommand
 {
-    public int Property;
+    public Property Property;
 }
 
 public struct AutomationSendUserEventCommand
@@ -94,8 +113,7 @@ public struct SineBankWave
 
 public struct SineBankConfig
 {
-[MarshalAs(UnmanagedType.ByValArray)]
-    public SineBankWave[] Waves;
+    public Span<SineBankWave> Waves;
     public ulong WaveCount;
     public double InitialFrequency;
 }
@@ -224,12 +242,12 @@ public enum AutomationCommand
     ClearAllProperties
 }
 
-public delegate void UserDataFreeCallback(byte[] data);
-public delegate int StreamReadCallback(out ulong read, ulong requested, out byte[] destination, byte[]? userdata, out string errmsg);
-public delegate int StreamSeekCallback(ulong pos, byte[]? userdata, out string errmsg);
-public delegate int StreamCloseCallback(byte[]? userdata, out string errmsg);
-public delegate int StreamDestroyCallback(byte[]? userdata);
-public delegate int StreamOpenCallback(CustomStreamDefinition callbacks, string protocol, string path, byte[]? param, byte[]? userdata, out string errmsg);
+public delegate void UserDataFreeCallback(Span<byte> data);
+public delegate int StreamReadCallback(out ulong read, ulong requested, out Span<byte> destination, Span<byte>? userdata, out string errmsg);
+public delegate int StreamSeekCallback(ulong pos, Span<byte>? userdata, out string errmsg);
+public delegate int StreamCloseCallback(Span<byte>? userdata, out string errmsg);
+public delegate int StreamDestroyCallback(Span<byte>? userdata);
+public delegate int StreamOpenCallback(CustomStreamDefinition callbacks, string protocol, string path, Span<byte>? param, Span<byte>? userdata, out string errmsg);
 
 public struct CustomStreamDefinition
 {
@@ -238,14 +256,13 @@ public struct CustomStreamDefinition
     public StreamCloseCallback Close;
     public StreamDestroyCallback Destroy;
     public long length;
-    [MarshalAs(UnmanagedType.ByValArray)]
-    public byte[]? userdata;
+    public Span<byte>? userdata;
 }
 
-public interface IRawSynthizer
+public interface IRawSynthizer : IDisposable
 {
     void syz_getVersion(out ulong major, out ulong minor, out ulong patch);
-    void syz_eventDeinit(ref Event evt);
+    void syz_eventDeinit(ref AutomationEvent evt);
     void syz_libraryConfigSetDefaults(out LibraryConfig config);
     int syz_initialize();
     int syz_initializeWithConfig(ref LibraryConfig config);
@@ -256,9 +273,9 @@ public interface IRawSynthizer
     int syz_handleDecRef(int handle);
     void syz_initDeleteBehaviorConfig(out DeleteBehaviorConfig config);
     int syz_configDeleteBehavior(int obj, ref DeleteBehaviorConfig cfg);
-    int syz_handleGetObjectType(out int type, int handle);
-    int syz_handleGetUserdata(out byte[] data, int handle);
-    int syz_handleSetUserdata(int handle, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_handleGetObjectType(out ObjectType type, int handle);
+    int syz_handleGetUserdata(out Span<byte> data, int handle);
+    int syz_handleSetUserdata(int handle, Span<byte>? userdata, UserDataFreeCallback? callback);
     int syz_pause(int handle);
     int syz_play(int handle);
     int syz_getI(out int value, int handle, Property property);
@@ -276,49 +293,52 @@ public interface IRawSynthizer
     int syz_biquadDesignLowpass(out BiquadConfig filter, double frequency, double q);
     int syz_biquadDesignHighpass(out BiquadConfig filter, double frequency, double q);
     int syz_biquadDesignBandpass(out BiquadConfig filter, double frequency, double bw);
-    int syz_createContext(out int handle, byte[]? userdata, UserDataFreeCallback? UserdataFreeCallback);
-    int syz_createContextHeadless(out int handle, byte[]? userdata, UserDataFreeCallback? UserdataFreeCallback);
-    int syz_contextGetBlock(int handle, out float[] block);
+    int syz_createContext(out int handle, Span<byte>? userdata, UserDataFreeCallback? UserdataFreeCallback);
+    int syz_createContextHeadless(out int handle, Span<byte>? userdata, UserDataFreeCallback? UserdataFreeCallback);
+    int syz_contextGetBlock(int handle, out Memory<float> block);
     int syz_contextEnableEvents(int handle);
-    int syz_contextGetNextEvent(out Event evt, int context, ulong flags);
-    int syz_registerStreamProtocol(string protocol, StreamOpenCallback callback, byte[]? userdata);
-    int syz_createStreamHandleFromStreamParams(out int handle, string protocol, string path, byte[]? param, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamHandleFromMemory(out int handle, ulong len, byte[] data, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamHandleFromFile(out int handle, string path, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamHandleFromCustomStream(out int handle, ref CustomStreamDefinition callbacks, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamingGeneratorFromStreamParams(out int handle, int context, string protocol, string path, byte[]? param, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamingGeneratorFromFile(out int handle, int context, string path, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createStreamingGeneratorFromStreamHandle(out int handle, int context, int stream_handle, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createBufferFromStreamParams(out int handle, string protocol, string path, byte[]? param, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createBufferFromEncodedData(out int handle, ulong len, byte[] data, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createBufferFromFloatArray(out int handle, uint sr, uint channels, ulong frames, float[] data, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createBufferFromFile(out int handle, string path, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createBufferFromStreamHandle(out int handle, int stream, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_contextGetNextEvent(out AutomationEvent evt, int context, ulong flags);
+    int syz_registerStreamProtocol(string protocol, StreamOpenCallback callback, Span<byte>? userdata);
+    int syz_createStreamHandleFromStreamParams(out int handle, string protocol, string path, Span<byte>? param, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamHandleFromMemory(out int handle, ulong len, Memory<byte> data, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamHandleFromFile(out int handle, string path, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamHandleFromCustomStream(out int handle, ref CustomStreamDefinition callbacks, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamingGeneratorFromStreamParams(out int handle, int context, string protocol, string path, Span<byte>? param, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamingGeneratorFromFile(out int handle, int context, string path, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createStreamingGeneratorFromStreamHandle(out int handle, int context, int stream_handle, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferFromStreamParams(out int handle, string protocol, string path, Span<byte>? param, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferFromEncodedData(out int handle, ulong len, Memory<byte> data, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferFromFloatArray(out int handle, uint sr, uint channels, ulong frames, Memory<float> data, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferFromFile(out int handle, string path, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferFromStreamHandle(out int handle, int stream, Span<byte>? userdata, UserDataFreeCallback? callback);
     int syz_bufferGetChannels(out uint channels, int handle);
     int syz_bufferGetLengthInSamples(out uint samples, int handle);
     int syz_bufferGetLengthInSeconds(out double seconds, int handle);
     int syz_bufferGetSizeInBytes(out ulong size, int handle);
-    int syz_createBufferGenerator(out int handle, int context, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_createBufferGenerator(out int handle, int context, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
     int syz_sourceAddGenerator(int source, int generator);
     int syz_sourceRemoveGenerator(int source, int generator);
-    int syz_createDirectSource(out int handle, int context, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createAngularPannedSource(out int handle, int context, PannerStrategy strategy, double azimuth, double elevation, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createScalarPannedSource(out int handle, int context, PannerStrategy strategy, double scalar, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createSource3D(out int handle, int context, PannerStrategy strategy, double x, double y, double z, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createNoiseGenerator(out int handle, int context, uint channels, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_createDirectSource(out int handle, int context, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createAngularPannedSource(out int handle, int context, PannerStrategy strategy, double azimuth, double elevation, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createScalarPannedSource(out int handle, int context, PannerStrategy strategy, double scalar, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createSource3D(out int handle, int context, PannerStrategy strategy, double x, double y, double z, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createNoiseGenerator(out int handle, int context, uint channels, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
     void syz_initSineBankConfig(out SineBankConfig cfg);
-    int syz_createFastSineBankGenerator(out int handle, int context, ref SineBankConfig cfg, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createFastSineBankGeneratorSine(out int handle, int context, double initial_frequency, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createFastSineBankGeneratorTriangle(out int handle, int context, double initial_frequency, uint partials, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createFastSineBankGeneratorSquare(out int handle, int context, double initial_frequency, uint partials, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_createFastSineBankGeneratorSaw(out int handle, int context, double initial_frequency, uint partials, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_createFastSineBankGenerator(out int handle, int context, ref SineBankConfig cfg, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createFastSineBankGeneratorSine(out int handle, int context, double initial_frequency, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createFastSineBankGeneratorTriangle(out int handle, int context, double initial_frequency, uint partials, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createFastSineBankGeneratorSquare(out int handle, int context, double initial_frequency, uint partials, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createFastSineBankGeneratorSaw(out int handle, int context, double initial_frequency, uint partials, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
     void syz_initRouteConfig(out RouteConfig cfg);
     int syz_routingConfigRoute(int context, int output, int input, ref RouteConfig config);
     int syz_routingRemoveRoute(int context, int output, int input, double fade_out);
     int syz_routingRemoveAllRoutes(int context, int output, double fade_out);
     int syz_effectReset(int handle);
-    int syz_createGlobalEcho(out int handle, int context, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
-    int syz_globalEchoSetTaps(int handle, uint len, EchoTapConfig[] taps);
-    int syz_createGlobalFdnReverb(out int handle, int context, byte[]? config, byte[]? userdata, UserDataFreeCallback? callback);
+    int syz_createGlobalEcho(out int handle, int context, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_globalEchoSetTaps(int handle, uint len, Memory<EchoTapConfig> taps);
+    int syz_createGlobalFdnReverb(out int handle, int context, Span<byte>? config, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_createAutomationBatch(out int handle, int context, Span<byte>? userdata, UserDataFreeCallback? callback);
+    int syz_automationBatchAddCommands(int batch, ulong commands_count, Memory<AutomationCommandData> commands);
+    int syz_automationBatchExecute(int batch);
 }
 
